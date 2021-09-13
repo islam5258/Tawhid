b240ab53c0c20e67d41bff4aa8b4445471394b3d
islam5258 <mohammed.software.testing@gmail.com>
2021-09-12 20:23:51 -0400

    example commit

--- src/mouse/action/test/SmokeTest.java
+++ src/mouse/action/test/SmokeTest.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.WebDriver;
 public class SmokeTest {
 
 	public static void main(String[] args) throws InterruptedException {
-		BaseTest b=new BaseTest();
-		WebDriver dr=b.openBrowser();
-		Mouse_Action ma=new Mouse_Action(dr);
+		//BaseTest b=new BaseTest();
+		//WebDriver dr=b.openBrowser();
+		//Mouse_Action ma=new Mouse_Action(dr);
+        Demo d=new Demo();
+        d.demo_m();
 		//ma.clickAndHold();
 		//ma.doubleClick();
 		//ma.moveToElement();
